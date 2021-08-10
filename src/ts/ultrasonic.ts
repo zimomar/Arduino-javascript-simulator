d9e428ca61de41a09c72e7454aa4e97ee2a587a3
ABOUSLIME Omar <21800354@etu.unicaen.fr>
2021-08-10 12:36:44 +0200

    ajout potentiometer mais lecture digital ne marche pas, trouver solution pour avoir le port A5 sur le simulateur

--- src/ts/ultrasonic.ts
+++ src/ts/ultrasonic.ts
@@ -1,13 +1,54 @@
 import { buildHex } from "./compile";
 import "@p4labs/elements";
 import { ArduinoIDEContainer } from "@p4labs/elements";
+import { ArduinoUnoElement, PotentiometerElement } from "@wokwi/elements";
+import { ArduinoUno } from "@p4labs/hardware";
+import { Component } from "@p4labs/hardware/dist/esm/Component";
+
+class PotentiometerComponent extends Component {
+  potentiometerElement: PotentiometerElement;
+  constructor(
+    pin: number,
+    label: string,
+    potentiometerElement: PotentiometerElement
+  ) {
+    super(pin, label);
+    this.potentiometerElement = potentiometerElement;
+  }
+  setValue(value: number) {
+    if (value <= 100 && value >= 0) {
+      this.potentiometerElement.value = value;
+    }
+  }
+  reset() {
+    this.potentiometerElement.value = 0;
+  }
+}
+
+const arduinoElement: ArduinoUnoElement = document.querySelector(
+  "#setup-workshop-wokwi-arduino"
+);
+
+const potentiometerElement: PotentiometerElement = document.querySelector("#potentiometer1");
+
+const potentiometerComponent = new PotentiometerComponent(
+  5,
+  "pot1",
+  potentiometerElement
+);
+
+const unoBoard = new ArduinoUno();
+
+//ledComponent.update(true);
+//ledComponent.reset();
+
+if (arduinoElement) unoBoard.setUnoElement(arduinoElement);
+
+unoBoard.addConnection(6, potentiometerComponent);
 
 let editor: any; // eslint-disable-line @typescript-eslint/no-explicit-any
 let simulationStatus = "off";
 
-// Load Editor
-declare const window: any; // eslint-disable-line @typescript-eslint/no-explicit-any
-declare const monaco: any; // eslint-disable-line @typescript-eslint/no-explicit-any
 window.require.config({
   paths: {
     vs: "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.20.0/min/vs",
@@ -15,40 +56,21 @@ window.require.config({
 });
 window.require(["vs/editor/editor.main"], () => {
   editor = monaco.editor.create(
-    document.querySelector("#ultrasonic-workshop-monaco"),
+    document.querySelector("#setup-workshop-monaco"),
     {
-      value: `const int pingPin = 5; // Trigger Pin of Ultrasonic Sensor
-const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
+      value: `int potentiometer = 5;    // select the input pin for the potentiometer
+      int BUILTIN_LED = 13;   // select the builtin LED
+      void setup() {
+       pinMode(BUILTIN_LED, OUTPUT);  // declare the led pin as an OUTPUT
+      }
+      void loop() {
+        int val = digitalRead(potentiometer);    // read the value from the sensor
+        digitalWrite(BUILTIN_LED, HIGH);  // turn on the led
+        delay(val);                  // wait for the select time from potentiometer
+        digitalWrite(BUILTIN_LED, LOW);   // turn off the led
+        delay(val);                  // wait for the select time from potentiometer
+      }
 
-void setup() {
-  //set up the Serial
-  Serial.begin(9600);
-  //setupt the pin modes  
-  pinMode(pingPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
-}
-
-void loop() {
-  //create a variable to save the duration in
-  long duration;  
-  //clear the ping pin
-  digitalWrite(pingPin, LOW);
-  delayMicroseconds(2);
-  //send the 10 microsecond trigger
-  digitalWrite(pingPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(pingPin, LOW);
-  //get the pulse duration in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  Serial.println(duration);
-
-  /*
-    TASK: Find the distance in cm at 
-          every robot position.
-  */
-  delay(1000);
-}
 `,
       language: "cpp",
       minimap: { enabled: false },
@@ -57,35 +79,15 @@ void loop() {
   );
 });
 
-const compilerOutputText = document.querySelector(
-  "#ultrasonic-compiler-output-text"
-);
-const serialOutputText = document.querySelector(
-  "#ultrasonic-serial-output-text"
-);
+const compilerOutputText = document.querySelector("#compiler-output-text");
+const serialOutputText = document.querySelector("#serial-output-text");
 
 const arduinoContainer = document.querySelector<ArduinoIDEContainer>(
-  "#ultrasonic-workshop-ide-container"
+  "#setup-workshop-ide-container"
 );
 
-//set up robot environment
-const canvas = document.getElementById("ultrasonic-world");
-
-const robot = new Robots.Arduino.TwoServoRobot(
-  canvas,
-  serialOutputText,
-  arduinoContainer,
-  "imgs/room-background.jpg"
-);
-robot.environment.robotInitialPosition = { x: 350, y: 200 };
-robot.environment.addObstacleRectangle(0, 400, 20, 800);
-robot.environment.addObstacleRectangle(800, 400, 20, 800);
-robot.environment.addObstacleRectangle(400, 0, 800, 20);
-robot.environment.addObstacleRectangle(400, 800, 800, 20);
-
-robot.environment.addObstacleRectangle(400, 100, 300, 100, "#3CAEA3");
-
-robot.environment.reset();
+unoBoard.setSerialOutputElement(serialOutputText);
+unoBoard.setTimeLabelElement(arduinoContainer);
 
 async function compileAndRun() {
   if (serialOutputText) serialOutputText.textContent = "";
@@ -97,7 +99,7 @@ async function compileAndRun() {
         if (compilerOutputText) compilerOutputText.textContent = "";
 
         simulationStatus = "on";
-        robot.run(result.hex);
+        unoBoard.executeProgram(result.hex);
       } else {
         simulationStatus = "off";
         if (arduinoContainer) arduinoContainer.status = "off";
@@ -113,7 +115,7 @@ async function compileAndRun() {
 }
 
 function stopCode() {
-  robot.stop();
+  unoBoard.stopExecute();
 }
 
 function handleIDEStatusChange(e: CustomEvent) {
@@ -125,26 +127,6 @@ function handleIDEStatusChange(e: CustomEvent) {
   }
   simulationStatus = status;
 }
-
 arduinoContainer?.addEventListener("_status-change", (e: CustomEvent) =>
   handleIDEStatusChange(e)
 );
-
-//set up the buttons
-const btnPositin1 = document.querySelector<ArduinoIDEContainer>("#position1");
-btnPositin1.addEventListener("click", () => {
-  robot.environment.setRobotPosition({ x: 350, y: 200 });
-  console.log("hello");
-});
-
-const btnPositin2 = document.querySelector<ArduinoIDEContainer>("#position2");
-btnPositin2.addEventListener("click", () => {
-  robot.environment.setRobotPosition({ x: 350, y: 250 });
-  console.log("hello");
-});
-
-const btnPositin3 = document.querySelector<ArduinoIDEContainer>("#position3");
-btnPositin3.addEventListener("click", () => {
-  robot.environment.setRobotPosition({ x: 350, y: 300 });
-  console.log("hello");
-});
